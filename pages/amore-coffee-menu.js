8b30d878e801f21211f0e599e3b22375437aa1c9
latz09 <85132328+latz09@users.noreply.github.com>
2022-09-21 11:50:13 -0500

    chalkboard

--- pages/amore-coffee-menu.js
+++ pages/amore-coffee-menu.js
@@ -1,6 +1,6 @@
 const menu = () => {
     return ( <div className="max-w-4xl mx-auto my-12">
-        <div className="bg-gradient-to-r from-[#212121] via-[#212121] to-[#03210d] min-h-screen border-8 border-gray-800">
+        <div className="bg-gradient-to-r from-[#212121] via-[#212121] to-[#191919] min-h-screen border-2 border-gray-900 shadow-xl">
 <h1 className="text-center font-amoreCursive text-white py-8 text-5xl">Amore Coffee</h1>
         </div>
         menu
